832c530cc5bdd98973c06493fb4dfd1b53a0d0d5
Samuel Palacios <samuelitopr77@gmail.com>
2026-04-13 17:16:30 -0500

    implementar clase Guest como dataclass

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -1,9 +1,19 @@
 from dataclasses import dataclass, field
-from datetime import datetime, date, timedelta
-from typing import ClassVar
+from datetime import date
+from typing import List
+from app.services.util import generate_unique_id, guest_not_found_error
 
-from app.services.util import (generate_unique_id, date_lower_than_today_error,
-    reservation_not_found_error, guest_not_found_error, room_not_available_error,
-    room_not_found_error, room_already_exists_error)
+class Guest:
+    # Variables de clase (Constantes)
+    REGULAR: str = "regular"
+    VIP: str = "vip"
 
+    # Atributos obligatorios
+    name: str
+    email: str
 
+    # Atributo opcional con valor por defecto de la clase
+    type_: str = REGULAR
+
+    def __str__(self) -> str:
+        return f"Guest {self.name} ({self.email}) of type {self.type_}"
